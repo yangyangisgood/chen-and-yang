fe9986a12e51ac8f578abd87e00b8f8422f8d0db
yang <s107048237@gmail.com>
2025-08-06 15:38:13 +0800

    feat: 首頁UI調整

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="container">
-    <h1>首頁</h1>
+    <div class="header">
+      <div></div>
+      <h1>首頁</h1>
+      <!-- 天數計數器 -->
+      <div id="days-counter">
+        <span id="days-count">D+{{ daysCount }}</span>
+      </div>
+    </div>
     <div class="button-container">
       <button @click="$router.push('/todo-list')">待辦清單</button>
       <button @click="$router.push('/dating-diary')">約會日記</button>
@@ -9,22 +16,6 @@
       <button @click="$router.push('/sushi-doesnt-go-round')">
         不迴轉壽司
       </button>
-    </div>
-
-    <!-- 天數計數器 -->
-    <div id="days-counter">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="calendar-icon"
-        viewBox="0 0 16 16"
-      >
-        <path
-          d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M2 2a1 1 0 0 0-1 1v1h14V3a1 1 0 0 0-1-1zm13 3H1v9a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1z"
-        />
-      </svg>
-      <span class="calendar-text">
-        <span id="days-count">D+{{ daysCount }}</span>
-      </span>
     </div>
   </div>
 </template>
@@ -58,13 +49,23 @@ onMounted(() => {
   background: linear-gradient(110deg, #eee 52%, #ccc 52%);
 }
 
-h1 {
-  text-align: center;
-  letter-spacing: 0.1em;
-  color: #333;
-  font-size: 1.6em;
-  margin: 1.4em 0 1em 0;
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 1em;
+  margin-top: 1.4em;
 }
+
+h1 {
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  text-align: center;
+  font-size: 1.6em;
+  color: #333;
+}
+
 .button-container {
   display: flex;
   flex-direction: column;
@@ -78,29 +79,13 @@ button {
   font-size: 1.6em;
   color: #333;
   background-color: #eee;
-  font-family: "sweiHalfMoonCJKtc-medium";
-}
-
-#days-counter {
-  position: fixed;
-  top: 8%;
-  right: 8%;
-}
-
-.calendar-icon {
-  height: 2.2em;
-  transform: scaleX(1.5);
-  fill: #333;
 }
 
 #days-count {
-  position: absolute;
-  top: 56%;
-  left: 50%;
-  transform: translate(-50%, -52%);
-  font-size: 0.72em;
+  font-size: 1.1em;
   color: #333;
   pointer-events: none;
   font-family: "SweiHalfMoonCJKtc-bold", Arial, sans-serif;
+  margin-right: 0.8em;
 }
 </style>
