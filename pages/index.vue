f28c40100fbb4b6f0ffa04d565c31e66e49220be
leo880730 <leov2880730@gmail.com>
2025-11-09 08:59:46 +0800

    feat: 移除吵架紀錄

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,7 @@
     <div class="button-container">
       <button @click="$router.push('/todo-list')">待辦清單</button>
       <button @click="$router.push('/dating-diary')">約會日記</button>
-      <button @click="$router.push('/discussion-record')">吵架紀錄冊</button>
+      <!-- <button @click="$router.push('/discussion-record')">吵架紀錄冊</button> -->
       <button @click="$router.push('/check-list')">關係檢查表</button>
       <button @click="$router.push('/sushi-doesnt-go-round')">
         不迴轉壽司
